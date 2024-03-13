cf123c6f410868f49e7d66354882715605a0cc92
paul <student11@human.com>
2024-03-13 15:59:07 +0900

    commit& push

---
+++ ex02/src/main/java/org/zerock/service/ReplyService.java
@@ -0,0 +1,17 @@
+package org.zerock.service;
+
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+
+import java.util.List;
+
+public interface ReplyService {
+    int register(ReplyVO vo);
+    int getCurrVal();
+    ReplyVO get(Long rno);
+    int modify(ReplyVO vo);
+    int remove(Long rno);
+    List<ReplyVO> getList(Criteria cri, Long bno);
+
+    int getTotalCount(Long bno);
+}
