69bbc5da4c5b4a90f2cfbdaf67d4ea70e95923e3
paul <student11@human.com>
2024-03-28 12:40:40 +0900

    123

--- java-start/src/CommentJava.java
+++
@@ -1,6 +0,0 @@
-public class CommentJava {
-    public static void main(String[] args) {
-        System.out.println("hello 1");
-        System.out.println("hello 2");
-    }
-}
--- zerock/src/main/java/org/zerock/domain/EmployeeDTO.java
+++
@@ -1,6 +0,0 @@
-package org.zerock.domain;
-
-public class EmployeeDTO {
-    private String name;
-    private int age;
-}
